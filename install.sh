4fd00c63e4c5f7fd8ec0df3333e6d1d69e1af1d1
Viktor Nagy <126671+nagyv@users.noreply.github.com>
2025-08-18 13:57:58 +0200

    Install claude-code-docs

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 bash ./bin/copy_dotfiles.sh
+
+# Install claude code docs https://github.com/ericbuess/claude-code-docs/
+curl -fsSL https://raw.githubusercontent.com/ericbuess/claude-code-docs/main/install.sh | bash
